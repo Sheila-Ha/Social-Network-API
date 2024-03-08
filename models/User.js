ed02238bb1ffb12bec86175311d482db52223da3
Sheila Hanson <slhanson11@live.com>
2024-03-08 10:16:10 -0600

    added comments to model/user.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// Schema to create User model
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -27,17 +28,22 @@ const userSchema = new mongoose.Schema({
     ],
   },
   {
+    // Two schema options to transform Objects after querying MongoDB: toJSON and toObject
+    // We want virtuals to be included in our response, overriding the default behavior
     toJSON: {
       virtuals: true,
     },
     id: false,
   }
 );
-
-userSchema.virtual("friendCount").get(function (){
+// Create a virtual property `commentCount` that get the amount of comments per user
+userSchema.virtual("friendCount")
+// Getter
+.get(function (){
   return this.friend.length;
 });
 
+//Initialize our User model
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
