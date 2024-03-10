bd2d3af0ab57aaf9031758b083ea6556d1fe2f4e
Sheila Hanson <slhanson11@live.com>
2024-03-09 22:20:00 -0600

    started test.js

---
+++ utils/test.js
@@ -0,0 +1,14 @@
+const dateFormat = require ('./dateFormatter');
+
+console.log(dateFormat(Date.now()));
+
+const friends = [
+    'Amy',
+    'Ann',
+    'Amos',
+    'Beth',
+    'Bob',
+    'Bill',
+    ' '
+
+]
